0df92cad1260c1572fa0874210b2d13040a7b24b
Mateusz Nadobnik <mateusz.nadobnik@bzwbk.pl>
2017-12-20 16:53:25 +0100

    Indexes

--- Indexes/MAINTENANCE.md
+++ Indexes/MAINTENANCE.md
@@ -3,4 +3,5 @@
 
 ### Links
 
---[Safely Dropping Unused SQL Server Indexes](https://www.mssqltips.com/sqlservertip/5202/safely-dropping-unused-sql-server-indexes/): Ciekawy wpis który porusza kwestie usuwania nieużywanych indeksów i problemów z którymi trzeba się zmierzyć
+--[Safely Dropping Unused SQL Server Indexes](https://www.mssqltips.com/sqlservertip/5202/safely-dropping-unused-sql-server-indexes/)
+ Ciekawy wpis który porusza kwestie usuwania nieużywanych indeksów i problemów z którymi trzeba się zmierzyć
--- Indexes/Scripts/script.sql
+++ Indexes/Scripts/script.sql
@@ -1,6 +1,10 @@
-# # # # # # Links - - [Safely Dropping Unused SQL Server Indexes](
-    https: / / www.mssqltips.com / sqlservertip / 5202 / safely - dropping - unused - sql - server - indexes /
-): Ciekawy wpis kt ó ry porusza kwestie usuwania nieu ż ywanych indeks ó w i problem ó w z kt ó rymi trzeba si ę zmierzy ć    i.[name],
+SELECT
+    DB_NAME() AS [database_name],
+    DB_ID() AS database_id,
+    OBJECT_SCHEMA_NAME(i.[object_id]) AS [schema_name],
+    OBJECT_NAME(i.[object_id]) AS [object_name],
+    iu.[object_id],
+    i.[name],
     i.index_id,
     i.[type_desc],
     i.is_primary_key,
@@ -11,15 +15,17 @@
     iu.user_lookups,
     iu.user_updates,
     iu.user_seeks + iu.user_scans + iu.user_lookups AS total_uses,
-    CASE WHEN (iu.user_seeks + iu.user_scans + iu.user_lookups) > 0
-        THEN iu.user_updates/( iu.user_seeks + iu.user_scans + iu.user_lookups )
-        ELSE iu.user_updates END AS update_to_use_ratio
-FROM sys.dm_db_index_usage_stats iu
-    RIGHT JOIN sys.indexes i ON iu.index_id = i.index_id AND iu.[object_id] = i.[object_id]
-WHERE 
-   OBJECTPROPERTY(iu.[object_id], 'IsUserTable') = 1
+    CASE WHEN (iu.user_seeks + iu.user_scans + iu.user_lookups) > 0 THEN iu.user_updates /(
+        iu.user_seeks + iu.user_scans + iu.user_lookups
+    ) ELSE iu.user_updates END AS update_to_use_ratio
+FROM
+    sys.dm_db_index_usage_stats iu
+    RIGHT JOIN sys.indexes i ON iu.index_id = i.index_id
+    AND iu.[object_id] = i.[object_id]
+WHERE
+    OBJECTPROPERTY(iu.[object_id], 'IsUserTable') = 1
     AND iu.database_id = DB_ID()
-ORDER BY 
-   CASE WHEN (iu.user_seeks + iu.user_scans + iu.user_lookups) > 0
-        THEN iu.user_updates/( iu.user_seeks + iu.user_scans + iu.user_lookups )
-        ELSE iu.user_updates END DESC 
+ORDER BY
+    CASE WHEN (iu.user_seeks + iu.user_scans + iu.user_lookups) > 0 THEN iu.user_updates /(
+        iu.user_seeks + iu.user_scans + iu.user_lookups
+    ) ELSE iu.user_updates END DESC
